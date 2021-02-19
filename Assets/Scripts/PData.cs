512d5ea6be5192636fcee3ad030178e2d0392de8
qui16000158 <55698561+qui16000158@users.noreply.github.com>
2021-02-19 22:40:22 +0000

    Added Player Data

---
+++ Assets/Scripts/PData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This is the container class which is serialized, this can be saved to, and loaded from
+[System.Serializable]
+public class PData
+{
+    public float posX, posY, posZ;
+    public Vector3 Position {
+        get
+        {
+            return new Vector3(posX, posY, posZ);
+        }
+        set
+        {
+            posX = value.x;
+            posY = value.y;
+            posZ = value.z;
+        }
+    }
+}
---
+++ Assets/Scripts/PDataManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class PDataManager : MonoBehaviour
+{
+    public static PDataManager instance;
+
+    public string FilePath
+    {
+        get
+        {
+            return Application.persistentDataPath + "/" + fileName;
+        }
+    }
+
+    public string fileName = "pdata.bin";
+
+    // Awake is called before Start
+    void Awake()
+    {
+        instance = this;
+    }
+
+    // This will load a player's data from disk, and return it
+    public PData Load()
+    {
+        if (File.Exists(FilePath))
+        {
+            FileStream stream = new FileStream(FilePath, FileMode.Open);
+            
+            BinaryFormatter bin = new BinaryFormatter();
+            // The binary formatter stores objects as bits, and therefore
+            // must be explicitly casted back to the correct object
+            return (PData)bin.Deserialize(stream);
+        }
+        else
+        {
+            return new PData();
+        }
+    }
+
+    // This will take a player's data, and save it to disk
+    public void Save(PData toSave)
+    {
+        FileStream stream = new FileStream(FilePath, FileMode.Create);
+
+        BinaryFormatter bin = new BinaryFormatter();
+        // This will convert our object into binary data
+        // and allow it to be written to a file
+        bin.Serialize(stream, toSave);
+        stream.Close();
+    }
+}
