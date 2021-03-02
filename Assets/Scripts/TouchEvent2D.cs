a32ac518c7fddcad75ebc7b0ef515dc55a70b5dd
qui16000158 <55698561+qui16000158@users.noreply.github.com>
2021-03-02 13:45:18 +0000

    Added level room

---
+++ Assets/Scripts/TouchEvent2D.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TouchEvent2D : MonoBehaviour
+{
+    [SerializeField]
+    new string tag = "Player";
+    [SerializeField]
+    UnityEvent onTouch;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == tag)
+        {
+            onTouch.Invoke();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == tag)
+        {
+            onTouch.Invoke();
+        }
+    }
+}
